7273d78ef7f79e3bbf94ccfa18a3ae48503ed559
higorblands <higorblandes@gmail.com>
2021-07-13 23:49:36 -0300

    Developed PersonServiceTest

---
+++ src/test/java/com/github/personapi/service/PersonServiceTest.java
@@ -0,0 +1,47 @@
+package com.github.personapi.service;
+
+import com.github.personapi.dto.request.PersonDTO;
+import com.github.personapi.dto.response.MessageResponseDTO;
+import com.github.personapi.entity.Person;
+import com.github.personapi.repository.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.github.personapi.utils.PersonUtils.createFakeDTO;
+import static com.github.personapi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
---
+++ src/test/java/com/github/personapi/utils/PersonUtils.java
@@ -0,0 +1,40 @@
+package com.github.personapi.utils;
+
+import com.github.personapi.dto.request.PersonDTO;
+import com.github.personapi.entity.Person;
+
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+
+
+public class PersonUtils {
+    private static final String FIRST_NAME = "Rodrigo";
+    private static final String LAST_NAME = "Peleias";
+    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(LocalDate.from(LocalTime.now()))
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+}
